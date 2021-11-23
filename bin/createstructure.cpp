2772450e0a2653464b54465a38ff1af0c4f423c5
DavideC03 <help@castellanidavide.it>
2021-11-23 18:47:45 +0100

    debian v10.*

--- bin/createstructure.cpp
+++ bin/createstructure.cpp
@@ -1,55 +1,42 @@
 // Dependencies
 #include <bits/stdc++.h>
-#include "libraries/bin/createstructure_inputs.hpp"
-#include "libraries/bin/createstructure_help.hpp"
-#include "libraries/bin/createstructure_login.hpp"
-#include "libraries/bin/createstructure_setting.hpp"
-#include "libraries/bin/createstructure_debian.hpp"
+#include "global-libraries/bin/inputs.hpp"
+#include "local-libraries/help.hpp"
+#include "local-libraries/version.hpp"
+#include "local-libraries/login.hpp"
+#include "local-libraries/create.hpp"
 
 // using ...
 using namespace std;
-using json = nlohmann::json;
 
 // Definitions
-//#define DEBUG
+#define DEBUG
 
-// Code
 int main(int argc, char *argv[])
 {
-	/* Main: the start point of the code
-	 *
-	 * inputs:
-	 * 	- argc: the number of command-line arguments
-	 *	- argv: an array containing all command-line arguments
- 	 *
-	 * output:
-	 *	- a run code: if it works in the correct way it will return 0
-	 */
+	// Scan inputs
+	Inputs inputs(argc, argv);
 
-	inputs i(argc, argv);
-#ifdef DEBUG
-	cout << i.to_string() << endl;
-#endif // DEBUG
+	if (inputs.exist("--help"))
+	{
+		Help::openMan();
+		return 0;
+	}
 
-	help h(i, "createstructure");
-#ifdef DEBUG
-	cout << h.to_string() << endl;
-#endif // DEBUG
+	if (inputs.exist("--version"))
+	{
+		Version::getVersion();
+		return 0;
+	}
 
-	login l(i);
-#ifdef DEBUG
-	cout << l.to_string() << endl;
-#endif // DEBUG
+	if (inputs.exist("--login"))
+	{
+		Login::login(inputs);
+		return 0;
+	}
 
-	setting s;
-#ifdef DEBUG
-	cout << s.to_string() << endl;
-#endif // DEBUG
-
-	debian d(i, s);
-#ifdef DEBUG
-	cout << d.to_string() << endl;
-#endif // DEBUG
-
+	Create::elaborate(inputs);
 	return 0;
 }
+
+#undef DEBUG
---
+++ bin/local-libraries/settings.hpp
@@ -0,0 +1,142 @@
+/**
+ * settings.hpp
+ *
+ * Library for handling settings.
+ *
+ * @author Castellani Davide (@DavideC03)
+ */
+
+#ifndef SETTING
+// If not imported yet
+#define SETTING
+
+// Dependencies
+#include <bits/stdc++.h>
+#include "../global-libraries/bin/json.hpp"
+#include "../global-libraries/bin/emoji.hpp"
+
+// using ...
+using namespace std;
+using json = nlohmann::json;
+
+// Definitions
+#define DEBUG
+
+// Classes prototipe(s)
+class Settings
+{
+    /* Setting: manage the settings
+     */
+private:
+    inline static string filename =
+#ifdef DEBUG
+        string(".")
+#else  // DEBUG
+        string(getenv("HOME"))
+#endif // DEBUG
+        + "/createstructure.setting";
+    string username;
+    string token;
+
+public:
+    Settings(string username = "", string token = "");
+    bool load();
+    bool save();
+    string getUsername();
+    string getToken();
+};
+
+Settings::Settings(string username, string token)
+{
+    /**
+     * Constructor
+     *
+     * @param username: github username, default ""
+     * @param token: github token, default ""
+     */
+    Settings::username = username;
+    Settings::token = token;
+
+    if (Settings::username == "" || Settings::username.empty() || Settings::token == "" || Settings::token.empty())
+    {
+        if (!Settings::load())
+        {
+            cout << "Plase, do the login first" << endl;
+            exit(EXIT_SUCCESS);
+        }
+#ifdef DEBUG
+        cout << "Loaded settings" << endl;
+#endif // DEBUG
+    }
+    else
+    {
+        Settings::save();
+    }
+}
+
+bool Settings::load()
+{
+    /**
+     * Load settings from file
+     *
+     * @return: true if settings are loaded, false otherwise
+     */
+    try
+    {
+        ifstream file(Settings::filename);
+        json j;
+        file >> j;
+        Settings::username = j["username"];
+        Settings::token = j["token"];
+        return true;
+    }
+    catch (...)
+    {
+        return false;
+    }
+}
+
+bool Settings::save()
+{
+    /**
+     * Save settings to file
+     *
+     * @return: true if settings are saved, false otherwise
+     */
+    try
+    {
+        ofstream file(Settings::filename);
+        json j;
+        j["username"] = Settings::username;
+        j["token"] = Settings::token;
+        file << j;
+        return true;
+    }
+    catch (...)
+    {
+        return false;
+    }
+}
+
+string Settings::getUsername()
+{
+    /**
+     * Get the username
+     *
+     * @return: username
+     */
+    return Settings::username;
+}
+
+string Settings::getToken()
+{
+    /**
+     * Get the token
+     *
+     * @return: token
+     */
+    return Settings::token;
+}
+
+#undef DEBUG
+#endif
